00e7326a6d82742f07140cbee1037ab8d0c3551e
Rangel Reale <rangelspam@gmail.com>
2022-05-12 10:58:44 -0300

    JSON parsing from selection in detail window

--- src/DetailWindow.cpp
+++ src/DetailWindow.cpp
@@ -2,6 +2,10 @@
 
 #include <QIcon>
 #include <QVBoxLayout>
+#include <QMenu>
+#include <QJsonDocument>
+#include <QTextCursor>
+#include <QRegExp>
 
 DetailWindow::DetailWindow(QWidget *parent, QString message, QString source) :
 	QDialog(parent)
@@ -19,6 +23,8 @@ DetailWindow::DetailWindow(QWidget *parent, QString message, QString source) :
 		_source->setText(source);
 		_source->setMinimumWidth(600);
         _source->setMinimumHeight(450);
+		_source->setContextMenuPolicy(Qt::CustomContextMenu);
+		connect(_source, &QTextEdit::customContextMenuRequested, this, &DetailWindow::textEditContextMenu);
 		_tabs->addTab(_source, "Source");
 	}
 
@@ -26,10 +32,64 @@ DetailWindow::DetailWindow(QWidget *parent, QString message, QString source) :
 	_message->setText(message);
 	_message->setMinimumWidth(600);
     _message->setMinimumHeight(450);
+	_message->setContextMenuPolicy(Qt::CustomContextMenu);
+	connect(_message, &QTextEdit::customContextMenuRequested, this, &DetailWindow::textEditContextMenu);
 	_tabs->addTab(_message, "Text");
 
 	QVBoxLayout *layout = new QVBoxLayout(this);
 	layout->addWidget(_tabs);
 
 	setLayout(layout);
+}
+
+void DetailWindow::textEditContextMenu(const QPoint &point)
+{
+	if (point.isNull())
+		return;
+ 	
+	QTextEdit *te = qobject_cast<QTextEdit*>(sender());
+
+	QMenu *menu = te->createStandardContextMenu();
+	QAction *parseJSON = nullptr;
+	if (!te->textCursor().selectedText().isEmpty()) {
+		menu->addSeparator();
+    	parseJSON = menu->addAction("Try parsing &JSON");
+	}
+    QAction *selectedItem = menu->exec(te->mapToGlobal(point));
+	if (selectedItem)
+	{
+		if (selectedItem == parseJSON)
+		{
+			tryFormatJSON(te->textCursor().selectedText(), te);
+		}
+	}
+    delete menu;
+}
+
+void DetailWindow::tryFormatJSON(const QString &text, QTextEdit *output)
+{
+	QJsonDocument doc = tryParseJSON(text);
+	if (doc.isNull()) return;
+
+	output->moveCursor(QTextCursor::End);
+
+	output->append("\n");
+	output->append(QString("-").repeated(40));
+	output->append("\n");
+	output->append(doc.toJson(QJsonDocument::Indented));
+}
+
+QJsonDocument DetailWindow::tryParseJSON(const QString &text)
+{
+	QJsonDocument doc = QJsonDocument::fromJson(text.toLocal8Bit());
+	if (!doc.isNull()) return doc;
+
+	// try unquoting
+	QString unquoted(text);
+	unquoted.replace(QRegExp("\\\\(.)"), "\\1");
+
+	doc = QJsonDocument::fromJson(unquoted.toLocal8Bit());
+	if (!doc.isNull()) return doc;
+
+	return QJsonDocument();
 }
--- src/DetailWindow.h
+++ src/DetailWindow.h
@@ -9,6 +9,7 @@
 #include <QTextEdit>
 #include <QDialog>
 #include <QTabWidget>
+#include <QJsonDocument>
 
 class DetailWindow : public QDialog
 {
@@ -17,7 +18,11 @@ public:
 	DetailWindow(QWidget *parent, QString message, QString source);
 public Q_SLOTS:
 	//void copyToClipboard();
+	void textEditContextMenu(const QPoint &point);
 private:
+	void tryFormatJSON(const QString &text, QTextEdit *output);
+	QJsonDocument tryParseJSON(const QString &text);
+
 	QTabWidget *_tabs;
 	QTextEdit *_message;
 	QTextEdit *_source;
