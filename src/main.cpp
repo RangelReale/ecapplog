59bfb0aa5c4eef50a01ec2d49f1a0e8d0fd4c15b
Rangel Reale <rangelspam@gmail.com>
2025-04-16 16:46:22 -0300

    increase default font size

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ int main(int argc, char *argv[])
 	app.setOrganizationName("RangelReale");
 	app.setOrganizationDomain("rangelreale.com");
 	app.setApplicationName("ECAppLog");
+    QFont font = app.font();
+    font.setPointSize(16);
+    app.setFont(font);
 #ifdef Q_OS_DARWIN
 	// Use "fusion" style on Mac to have scrollable tabs in QTabWidget
 	app.setStyle(QStyleFactory::create("Fusion"));
